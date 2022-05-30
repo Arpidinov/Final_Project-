f6573996d17df190c61dfc9cc725f3eeb159d3f7
ymytbek0314@gmail.com <ymytbek0314@gmail.com>
2022-05-30 22:21:47 +0600

    added save option

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+
 from tkinter import Button, messagebox
 from tkinter import * 
 from PIL import Image,ImageTk
 from tkinter import filedialog as F 
 from tkinter import Canvas, Text
-import numpy as np 
+import numpy as np
+from sympy import limit 
+
+img = None 
+
 class PhotoEditor(Tk):
     def __init__(self):
         Tk.__init__(self)
@@ -30,37 +35,80 @@ class MainPage(Frame):
 class Black(Frame):
     def __init__ (self,main):
         Frame.__init__(self,main)
-        self.label=Label(self,text = "Black & White filter",)
+        self.label=Label(self,text = "Black & White filter")
         self.label.pack()
         
         self.configure()
-        Button(self,text = 'Return to main page',fg = 'red',command = lambda: main.switch_frame(MainPage)).pack()
+        s=Button(self,text = 'Return to main page',fg = 'red',command = lambda: main.switch_frame(MainPage))
+        s.pack(side=BOTTOM)
     def configure(self):
-        self.canvas = Canvas(self,width=650,height=350)
-        self.canvas.pack()
-        Button(self,text='Open image',command= self.open_image,).pack()
+        self.label = Label(self)
+        self.label.pack()
+        Button(self,text='Open image',command=lambda:self.open_image()).pack()
+        
 
-        Button(self,text='Apply filter',command=self.apply_filter).pack()
+        Button(self,text='Apply filter',command=lambda:self.apply_filter()).pack()
+        
     def open_image(self):
+        #self.label.config(image='')
 
         self.ifile = F.askopenfile(parent=self,mode='rb',title='Choose a file')
-        limit = (590,350)
+        self.limit = (450,350)
         if self.ifile:
             self.image = Image.open(self.ifile)
-            self.image.thumbnail(limit)
+            self.image.thumbnail(self.limit)
             self.image = ImageTk.PhotoImage(self.image)
-            self.canvas.create_image(200,150,anchor = 'center',image=self.image)
+            self.label.config(image=self.image)
+
+           
+         
           
-      
     def apply_filter(self):
-        #self.pack_forget()
+        #self.label.config(image='')
+       
+
+  
+        self.applied = Image.open(self.ifile)
+        self.applied.thumbnail(self.limit)
+        self.applied = self.applied.convert("L")
+        self.applied = ImageTk.PhotoImage(self.applied)
+        #self.canvas.create_image(20,20,anchor=NW,image=self.applied)
+        self.label.config(image=self.applied)
+
+        Button(self,text='Save a file',command=lambda: self.save_file()).pack()
+
+    def save_file(self):
+        file = F.asksaveasfile(mode='w',defaultextension='jpg')
+        if file:
+            pass
+        
+     
+     #(def undo(self):
+      #   self.limit = (450,350)
+       #  if self.ifile:
+        #    self.image = Image.open(self.ifile)
+         #   self.image.thumbnail(self.limit)
+          #  self.image = ImageTk.PhotoImage(self.image)
+           # self.label.config(image=self.image))
+           
+
+
+
+
+       
+        
+
+      
+    ''' #self.pack_forget()
         self.applied = Image.open(self.ifile)
         self.applied = self.applied.convert("L")
         self.applied = ImageTk.PhotoImage(self.applied)
-        self.canvas.create_image(200,150,anchor='center',image=self.applied)
+        self.canvas.create_image(200,150,anchor='center',image=self.applied)'''
+    
 
-
-
+        
+     
+ 
 
 if __name__ == '__main__':
     app = PhotoEditor()
--- scratch.py
+++ scratch.py
@@ -1,8 +1,31 @@
+
 from tkinter import *
+from PIL import Image, ImageTk
 
-root = Tk()
+SWH = Tk()
 
-b = Button(root, text="Delete me", command=lambda: b.pack_forget())
-b.pack()
+SWH.geometry("1024x950+130+0")
+SWH.title("ServiceWhiz.")
 
-root.mainloop()
+img = None
+
+def printimage():
+    global img
+    load = Image.open("alatoo.png")
+    render = ImageTk.PhotoImage(load)
+
+    img = Button(SWH, image=render,command=imgpress)
+    img.image = render
+    img.place(x=0,y=0)
+    return;
+
+def imgpress():
+    global img
+    img.destroy()
+    Label1 = Label(SWH, text="Image has been clicked",fg="#0094FF",font=('Arial',20)).pack()
+    return;
+
+SWTitle = Label(SWH, text="ServiceWhiz.",fg="#0094FF",font=('Arial',20)).pack()
+MyButtonTest = Button(SWH, text="Click Me.",fg="White",bg="#0094FF",command=printimage).pack()
+
+SWH.mainloop()
